d19d8477ffde63acf9455a08c0cf1426d2dffa78
Jack Case <jackacase@gmail.com>
2020-03-08 12:11:35 -0400

    reformatting, and working on record.py

--- beats.py
+++ beats.py
@@ -1,13 +1,14 @@
 import librosa as rosa
 import numpy as np
 
+
 def average_delta(ref_audio, uncal_audio, range=0.3):
     """
     return the average difference between each beat in the reference and uncalibrated track in seconds
     """
-    
-    refbeats, uncalbeats = match_beats(ref_audio,uncal_audio)
-    
+
+    refbeats, uncalbeats = match_beats(ref_audio, uncal_audio)
+
     diffsum = 0
     for ref, uncal in zip(refbeats, uncalbeats):
         diffsum = diffsum + (uncal - ref)
@@ -16,18 +17,20 @@ def average_delta(ref_audio, uncal_audio, range=0.3):
 
     return average
 
+
 def match_beats(ref_audio, uncal_audio, range=.3, sr=22050):
     """
     prune beats from the reference that don't have a corresponding beat
     in the uncalibrated audio track
     """
-    
+
     ref_beats = find_beats(ref_audio, sr).tolist()
     uncal_beats = find_beats(uncal_audio, sr).tolist()
 
     # only reference beats which have an uncalibrated beat nearby should be kept
-    pruned_reference_beats = [beat for beat in ref_beats 
-                                if len([unbeat for unbeat in uncal_beats if unbeat <= beat + range and unbeat >= beat - range]) > 0]
+    pruned_reference_beats = [beat for beat in ref_beats
+                              if len(
+            [unbeat for unbeat in uncal_beats if unbeat <= beat + range and unbeat >= beat - range]) > 0]
 
     uncal_beats = uncal_beats[:len(pruned_reference_beats)]
 
--- record.py
+++ record.py
@@ -1,4 +1,7 @@
 import sounddevice as sd
 
-def record():
-    pass
+
+def record(duration=10, sr=22050):
+    recording = sd.rec(duration * sr, sr, channels=1)
+    sd.wait()
+    return recording
